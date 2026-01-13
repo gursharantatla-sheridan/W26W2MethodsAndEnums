fb6779d85529021088f18b25ceda9d8976454ab4
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-13 18:39:27 -0500

    params

--- W26W2MethodsAndEnums/Program.cs
+++ W26W2MethodsAndEnums/Program.cs
@@ -13,15 +13,67 @@
             Console.WriteLine("s = " + s);
             Console.WriteLine("a[0] = " + a[0]);
 
-            ChangeValues(n, s, a);
+            ChangeValues(ref n, s, a);
 
             Console.WriteLine("\nAfter changes:");
             Console.WriteLine("n = " + n);
             Console.WriteLine("s = " + s);
             Console.WriteLine("a[0] = " + a[0]);
+
+
+            // output parameter example
+            int rad = 5;
+            //double area, circum;
+
+            AreaAndCircum(rad, out double area, out double circum);
+
+            Console.WriteLine("\n\nArea = " + area);
+            Console.WriteLine("Circum = " + circum);
+            Console.WriteLine("\n\n");
+
+
+            //Console.Write("Enter a number: ");
+
+            //if (int.TryParse(Console.ReadLine(), out int num))
+            //    Console.WriteLine("\nnum = " + num);
+            //else
+            //    Console.WriteLine("\nInvalid input, please try again");
+
+
+
+            // parameter arrays example
+            int x = 4, y = 6;
+            Console.WriteLine("x = " + x + " y = " + y);
+            Console.WriteLine($"x = {x} y = {y}");
+            Console.WriteLine("x = {0} y = {1}", x, y);
+
+
+            int[] myArr = { 3, 2, 4, 5, 6, 7, 87, 6, 4 };
+            PrintArray(myArr);
         }
 
-        static void ChangeValues(int num, string str, int[] arr)
+
+        // parameter array
+        static void PrintArray(int[] arr)
+        {
+            Console.WriteLine("\n\nTotal number of items: " + arr.Length);
+
+            foreach (int i in arr)
+                Console.Write(i + " ");
+            Console.WriteLine();
+        }
+
+
+        // output parameters
+        static void AreaAndCircum(int r, out double area, out double circum)
+        {
+            area = Math.PI * r * r;
+            circum = 2 * Math.PI * r;
+        }
+
+
+
+        static void ChangeValues(ref int num, string str, int[] arr)
         {
             num = 100;
             str = "bye";
